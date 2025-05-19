fa3004a4c2826d8374d540f1bcc23fde96af3f50
kisana <kisana@amiverse.net>
2025-05-19 12:01:16 +0900

    設定ファイル記述ミス修正

--- src/config/environments/production.rb
+++ src/config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     user_name: "apikey",
     password: Rails.application.credentials.dig(:smtp, :password),
-    domain: "anyur.com"
+    domain: "anyur.com",
     address: "smtp.sendgrid.net",
     port: 587,
     authentication: :plain,
