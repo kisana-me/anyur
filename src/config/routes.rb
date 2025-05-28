11d7895cb5858bf534b7c14f6f603a12975c5543
kisana <kisana@amiverse.net>
2025-05-28 23:59:16 +0900

    manifestルーティング修正

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -61,7 +61,7 @@ Rails.application.routes.draw do
 
   # others
   # get "up" => "rails/health#show", as: :rails_health_check
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest, defaults: { format: :json }
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # errors
