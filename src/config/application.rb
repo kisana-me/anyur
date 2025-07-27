987ba09dcfb3ed4a359c393c9df33cf4cbf40fff
kisana <kisana@amiverse.net>
2025-07-27 21:40:42 +0900

    FIX: 設定ファイルを整形

--- src/config/application.rb
+++ src/config/application.rb
@@ -9,7 +9,7 @@ module App
     config.load_defaults 8.0
     config.autoload_lib(ignore: %w[assets tasks])
     config.time_zone = "Tokyo"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
     config.action_view.field_error_proc = Proc.new do |html_tag, _instance|
       html_tag.html_safe
