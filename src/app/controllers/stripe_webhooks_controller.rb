5acb196cb3a23afd56e234618040821b147229e9
kisana <kisana@amiverse.net>
2025-08-01 10:41:59 +0900

    CHANGE: Stripe WHで'を"に

--- src/app/controllers/stripe_webhooks_controller.rb
+++ src/app/controllers/stripe_webhooks_controller.rb
@@ -4,8 +4,8 @@ class StripeWebhooksController < ApplicationController
 
   def create
     payload = request.body.read
-    sig_header = request.env['HTTP_STRIPE_SIGNATURE']
-    endpoint_secret = ENV['STRIPE_SIGNING_SECRET']
+    sig_header = request.env["HTTP_STRIPE_SIGNATURE"]
+    endpoint_secret = ENV["STRIPE_SIGNING_SECRET"]
     event = nil
 
     begin
@@ -21,16 +21,16 @@ class StripeWebhooksController < ApplicationController
     end
 
     case event.type
-    when 'checkout.session.completed'
+    when "checkout.session.completed"
       session = event.data.object
       handle_checkout_session_completed(session)
-    when 'customer.subscription.created'
+    when "customer.subscription.created"
       subscription = event.data.object
       handle_subscription_created(subscription)
-    when 'customer.subscription.updated'
+    when "customer.subscription.updated"
       subscription = event.data.object
       handle_subscription_updated(subscription)
-    when 'customer.subscription.deleted'
+    when "customer.subscription.deleted"
       subscription = event.data.object
       handle_subscription_deleted(subscription)
     else
@@ -55,14 +55,14 @@ class StripeWebhooksController < ApplicationController
 
     subscription = account.subscriptions.find_or_initialize_by(stripe_subscription_id: stripe_sub.id)
     subscription.assign_attributes(
-      stripe_plan_id: stripe_sub['items']['data'][0]['price']['id'],
-      subscription_status: stripe_sub['status'],
-      current_period_start: stripe_sub['items']['data'][0]['current_period_start'] ? Time.at(stripe_sub['items']['data'][0]['current_period_start']) : nil,
-      current_period_end: stripe_sub['items']['data'][0]['current_period_end'] ? Time.at(stripe_sub['items']['data'][0]['current_period_end']) : nil,
-      cancel_at_period_end: stripe_sub['cancel_at_period_end'],
-      canceled_at: stripe_sub['canceled_at'] ? Time.at(stripe_sub['canceled_at']) : nil,
-      trial_start_at: stripe_sub['trial_start'] ? Time.at(stripe_sub['trial_start']) : nil,
-      trial_end_at: stripe_sub['trial_end'] ? Time.at(stripe_sub['trial_end']) : nil
+      stripe_plan_id: stripe_sub["items"]["data"][0]["price"]["id"],
+      subscription_status: stripe_sub["status"],
+      current_period_start: stripe_sub["items"]["data"][0]["current_period_start"] ? Time.at(stripe_sub["items"]["data"][0]["current_period_start"]) : nil,
+      current_period_end: stripe_sub["items"]["data"][0]["current_period_end"] ? Time.at(stripe_sub["items"]["data"][0]["current_period_end"]) : nil,
+      cancel_at_period_end: stripe_sub["cancel_at_period_end"],
+      canceled_at: stripe_sub["canceled_at"] ? Time.at(stripe_sub["canceled_at"]) : nil,
+      trial_start_at: stripe_sub["trial_start"] ? Time.at(stripe_sub["trial_start"]) : nil,
+      trial_end_at: stripe_sub["trial_end"] ? Time.at(stripe_sub["trial_end"]) : nil
     )
     subscription.save!
   end
@@ -72,14 +72,14 @@ class StripeWebhooksController < ApplicationController
     return unless subscription
 
     subscription.assign_attributes(
-      stripe_plan_id: stripe_sub['items']['data'][0]['price']['id'],
-      subscription_status: stripe_sub['status'],
-      current_period_start: stripe_sub['items']['data'][0]['current_period_start'] ? Time.at(stripe_sub['items']['data'][0]['current_period_start']) : nil,
-      current_period_end: stripe_sub['items']['data'][0]['current_period_end'] ? Time.at(stripe_sub['items']['data'][0]['current_period_end']) : nil,
-      cancel_at_period_end: stripe_sub['cancel_at_period_end'],
-      canceled_at: stripe_sub['canceled_at'] ? Time.at(stripe_sub['canceled_at']) : nil,
-      trial_start_at: stripe_sub['trial_start'] ? Time.at(stripe_sub['trial_start']) : nil,
-      trial_end_at: stripe_sub['trial_end'] ? Time.at(stripe_sub['trial_end']) : nil
+      stripe_plan_id: stripe_sub["items"]["data"][0]["price"]["id"],
+      subscription_status: stripe_sub["status"],
+      current_period_start: stripe_sub["items"]["data"][0]["current_period_start"] ? Time.at(stripe_sub["items"]["data"][0]["current_period_start"]) : nil,
+      current_period_end: stripe_sub["items"]["data"][0]["current_period_end"] ? Time.at(stripe_sub["items"]["data"][0]["current_period_end"]) : nil,
+      cancel_at_period_end: stripe_sub["cancel_at_period_end"],
+      canceled_at: stripe_sub["canceled_at"] ? Time.at(stripe_sub["canceled_at"]) : nil,
+      trial_start_at: stripe_sub["trial_start"] ? Time.at(stripe_sub["trial_start"]) : nil,
+      trial_end_at: stripe_sub["trial_end"] ? Time.at(stripe_sub["trial_end"]) : nil
     )
     subscription.save!
   end
@@ -90,8 +90,8 @@ class StripeWebhooksController < ApplicationController
 
     subscription.update!(
       subscription_status: :canceled,
-      canceled_at: stripe_sub['canceled_at'] ? Time.at(stripe_sub['canceled_at']) : nil,
-      current_period_end: stripe_sub['items']['data'][0]['current_period_end'] ? Time.at(stripe_sub['items']['data'][0]['current_period_end']) : nil
+      canceled_at: stripe_sub["canceled_at"] ? Time.at(stripe_sub["canceled_at"]) : nil,
+      current_period_end: stripe_sub["items"]["data"][0]["current_period_end"] ? Time.at(stripe_sub["items"]["data"][0]["current_period_end"]) : nil
     )
   end
 end
