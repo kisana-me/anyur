309757c4a857da983838adf1c005b002b64aec86
kisana <kisana@amiverse.net>
2026-04-06 18:19:21 +0900

    FIX db

--- src/db/migrate/0000011_add_public_client_to_services.rb
+++
@@ -1,9 +0,0 @@
-class AddPublicClientToServices < ActiveRecord::Migration[8.0]
-  def up
-    add_column :services, :public_client, :boolean, null: false, default: false
-  end
-
-  def down
-    remove_column :services, :public_client
-  end
-end
